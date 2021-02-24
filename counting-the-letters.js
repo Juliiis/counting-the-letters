93ab72a782eadcb6b37d376370db2101796dec61
Julieta Martin <jumartinnob@gmail.com>
2021-02-24 17:04:36 +0100

    counting-the-letters solved

--- counting-the-letters.js
+++ counting-the-letters.js
@@ -1,2 +1,21 @@
 // Escribe una función que dado un input "aaaabbbbbcca" 
 // devuelva un output: [["a", 4], ["b", 5], ["c", 2], ["a", 1]]
+
+// input = "aaaabbbbbcca";
+// output = [["a", 4], ["b", 5], ["c", 2], ["a", 1]];
+
+function countingLetters(str){
+    let result = [];
+    let count = 0;
+
+    for(let i=1; i <= str.length ; i++){
+        if(str[i] === str[i - 1]){
+            count++;
+        } else {
+            result.push(str[i - 1], count)
+            count = 0;
+        }
+    }
+    return result;
+}
+console.log(countingLetters("aaaabbbbbcca"))
